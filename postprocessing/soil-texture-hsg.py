a298aa60fca7d599683769e8a05727df55721eee
Martin Landa <landa.martin@gmail.com>
2021-08-23 22:26:13 +0200

    soil-texture-hsg: support styles

--- postprocessing/soil-texture-hsg.py
+++ postprocessing/soil-texture-hsg.py
@@ -1,17 +1,19 @@
 import os
+from zipfile import ZipFile
 
 from qgis.core import QgsRasterLayer, QgsProject
-
-import processing
 
 class wps_postprocessing:
     def postprocess(self, inputs, response):
         process_identifier = os.path.splitext(os.path.basename(__file__))[0]
         try:
+            target_dir = os.path.splitext(response.output['output'].filepath)[0]
+            with ZipFile(response.output['output'].filepath, 'r') as zf:
+                zf.extractall(target_dir)
             for name in inputs["layers"].text().split(','):
-                uri = '/vsizip/' + response.output['output'].filepath + '/' + name + '.tif'
-                print(uri)
+                uri = os.path.join(target_dir, name + '.tif')
                 raster = QgsRasterLayer(uri, "{} {}".format(process_identifier, name))
+                raster.loadNamedStyle(os.path.join(target_dir, name + '.qml'))
                 QgsProject.instance().addMapLayer(raster)
         except Exception as e:
             print(e)
